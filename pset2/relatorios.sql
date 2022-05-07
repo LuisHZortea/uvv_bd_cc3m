e9c76a1749599dd5fb91d47110c6fc939baac9c1
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2022-05-07 15:56:52 -0300

    Update and rename ScriptSQL to relatorios.sql

---
+++ pset2/relatorios.sql
@@ -0,0 +1,13 @@
+--1ª Questão
+
+select avg(f.salario) as media_salarial, d.nome_departamento
+from funcionario f
+inner join departamento d
+on d.numero_departamento = f.numero_departamento
+group by d.nome_departamento;
+
+--2ª Questão
+
+select avg(f.salario) as media_salarial, f.sexo
+from funcionario f
+group by f.sexo;
