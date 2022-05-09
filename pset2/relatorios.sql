ef1560af0ada7c9efb593c829cd968cbae1f4d38
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2022-05-09 13:43:04 -0300

    Update relatorios.sql

--- pset2/relatorios.sql
+++ pset2/relatorios.sql
@@ -58,4 +58,40 @@ on f.numero_departamento = d.numero_departamento
 inner join gerente g on g.cpf = d.cpf_gerente
 order by d.nome_departamento asc, f.salario desc;
 
+-- 6ª Questão
 
+select concat(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) funcionário, dpt.nome_departamento Departamento, 
+concat(d.nome_dependente, ' ', f.nome_meio, ' ', f.ultimo_nome) dependente, timestampdiff(year, d.data_nascimento, curdate()) "Idade do Dependente",
+case
+when d.sexo = 'M' then "Masculino"
+when d.sexo = 'F' then "Feminino"
+end "Sexo do Dependente"
+from funcionario f
+inner join departamento dpt
+on f.numero_departamento = dpt.numero_departamento
+inner join dependente d
+on f.cpf = d.cpf_funcionario;
+
+-- 7ª Questão
+
+select concat(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) funcionário, dpt.nome_departamento departamento,
+concat("R$", ' ', f.salario) Salário
+from funcionario f
+left outer join dependente d
+on f.cpf = d.cpf_funcionario
+inner join departamento dpt
+on f.numero_departamento = dpt.numero_departamento
+where d.cpf_funcionario is null;
+
+-- 8ª Questão
+
+select dpt.nome_departamento departamento, p.nome_projeto projeto,
+concat(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) "Nome Completo", concat(t.horas, "h") Horas
+from funcionario f 
+inner join departamento dpt 
+inner join projeto p 
+inner join trabalha_em t
+where f.numero_departamento = dpt.numero_departamento 
+and p.numero_projeto = t.numero_projeto 
+and f.cpf = t.cpf_funcionario 
+order by p.numero_projeto;
